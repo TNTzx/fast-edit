d00596bb5854be52f3f1443edb42c6001fe98298
TNTzx <xanderandrewritua@gmail.com>
2022-03-03 22:17:15 +0800

    aa

--- backend/actions/speed_change/formats.py
+++ backend/actions/speed_change/formats.py
@@ -1,22 +1,41 @@
 """Module that contains formats for representing speed."""
 
 
-from re import L
-
-
 class SpeedFormat():
     """Base class for speed formats."""
     def __init__(self, value: int | float):
         self.name: str = None
+        self.prefix: str = None
         self.value = value
+    
+    def __repr__(self):
+        return f"{self.name} ({self.prefix})"
 
 
-class SubSpeedFormat(SpeedFormat):
-    """The sub-speed formats."""
-
-class MainDecimal(SpeedFormat):
-    """The main speed format."""
-    def __init__(self, value: SubSpeedFormat | int | float):
+class Decimal(SpeedFormat):
+    """Decimal."""
+    def __init__(self, value: int | float):
         super().__init__(value)
-        
+        self.name = "Decimal"
+        self.prefix = "."
 
+    def from_nonstandard(self, data):
+        """Function to call to convert other speed formats to this one."""
+        if isinstance(data, Percent):
+            self.value = data / 100
+
+
+class Percent(SpeedFormat):
+    """Percent."""
+    def __init__(self, value: int | float):
+        super().__init__(value)
+        self.name = "Percent"
+        self.prefix = "%"
+
+    def from_standard(self, data):
+        """Function to call to convert other speed formats to this one."""
+        self.value = data * 100
+
+
+DEFAULT = 0
+SPEED_FORMATS = [Decimal(DEFAULT), Percent(DEFAULT)]
--- backend/misc/other.py
+++ backend/misc/other.py
@@ -5,18 +5,23 @@ import unicodedata as ud
 import re
 
 
-def slugify(value, allow_unicode=False):
-    """
-    Taken from https://github.com/django/django/blob/master/django/utils/text.py
-    Convert to ASCII if 'allow_unicode' is False. Convert spaces or repeated
-    dashes to single dashes. Remove characters that aren't alphanumerics,
-    underscores, or hyphens. Convert to lowercase. Also strip leading and
-    trailing whitespace, dashes, and underscores.
-    """
-    value = str(value)
-    if allow_unicode:
-        value = ud.normalize('NFKC', value)
-    else:
-        value = ud.normalize('NFKD', value).encode('ascii', 'ignore').decode('ascii')
-    value = re.sub(r'[^\w\s-]', '', value.lower())
-    return re.sub(r'[-\s]+', '-', value).strip('-_')
+def get_dict_attr(obj):
+    """Gets attributes of an object then returns it as a dict."""
+    def check_if_has_dict(obj):
+        return hasattr(obj, "__dict__")
+
+    dictionary = {}
+    for attr, value in obj.__dict__.items():
+        if isinstance(value, list):
+            value_list = []
+            for value_item in value:
+                if not check_if_has_dict(value_item):
+                    value_list.append(value_item)
+                else:
+                    value_list.append(get_dict_attr(value_item))
+            dictionary[attr] = value_list
+        elif not check_if_has_dict(value):
+            dictionary[attr] = value
+        else:
+            dictionary[attr] = get_dict_attr(value)
+    return dictionary
